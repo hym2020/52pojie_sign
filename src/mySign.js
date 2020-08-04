507a490d8a3f479ee55d66d41851e7ba77ddaf13
hym2020 <66545285+hym2020@users.noreply.github.com>
2020-08-04 11:11:56 +0800

    Update mySign.js

--- src/mySign.js
+++ src/mySign.js
@@ -5,11 +5,11 @@ async function signUp(myLogger){
 	
 	try{
 		const rst = (await siteUtils.sendGetRequest(`${hostURL}/home.php?mod=task&do=apply&id=2`, {}, true, 'gb2312')).body
-		
-		if(/任务已成功完成/)
-			return Promise.resolve('成功簽到')
+		console.log(rst)
 		if(/本期您已申请过此任务/.test(rst))
 			return Promise.resolve('已經簽過')
+		if(/任务已成功完成/)
+			return Promise.resolve('成功簽到')
 		else
 			return Promise.resolve('未知內容，請稍後查看')
 		
